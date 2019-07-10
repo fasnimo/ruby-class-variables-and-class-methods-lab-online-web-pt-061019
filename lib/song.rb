62cd73a0ccede32ff9e3f1d221a3732444afedcf
Rene Cocom <fasnimo@gmail.com>
2019-07-10 17:59:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
 
-  # @@song_count = 0
+  # @@count = 0
   attr_reader :name, :artist, :genre
 
   def initialize(album_name, artist_name, genre_type)
@@ -10,12 +10,12 @@ class Song
   end
 
   # def initialize
-  #   @@song_count += 1
+  #   @@count += 1
   #
   # end
   #
   # def self.count
-  #     @@song_count
+  #     @@count
   # end
 
 
