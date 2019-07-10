2f5533f151d9c2b30c2dfbb8f9901ed061e13108
Rene Cocom <fasnimo@gmail.com>
2019-07-10 17:54:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,8 @@ class Song
   attr_reader :name, :artist, :genre
   def initialize(album_name, artist_name, genre_type)
     @name = album_name
+    @artist = artist_name
+    @genre = genre_type
   end
 
   # def name
